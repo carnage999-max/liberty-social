15d116aa2a265e72c5e3af0232a1811f4217f335
carnage999-max <jamesezekiel039@gmail.com>
2025-11-06 06:52:20 +0100

    backend: updated start command for celery worker

--- backend/start_worker.sh
+++ backend/start_worker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Running migrations..."
-./venv/bin/python manage.py migrate --noinput
-
 echo "Starting celery worker..."
 ./venv/bin/celery -A liberty_social worker --loglevel=info
