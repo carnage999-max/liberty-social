8aacbc39dbcace8a71fd82cdb648865953a56ae4
carnage999-max <jamesezekiel039@gmail.com>
2025-11-10 18:06:19 +0100

    chore: fixed channels routing

--- backend/liberty_social/routing.py
+++ backend/liberty_social/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from main.consumers import ChatConsumer, NotificationConsumer
 
 websocket_urlpatterns = [
-    path("ws/chat/<uuid:conversation_id>/", ChatConsumer.as_asgi()),
+    path("ws/chat/<int:conversation_id>/", ChatConsumer.as_asgi()),
     path("ws/notifications/", NotificationConsumer.as_asgi()),
 ]
